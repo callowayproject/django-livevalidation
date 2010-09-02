82639eeb22b6b65051a7296838055d5f4fd82d09
Justin Quick <justquick@gmail.com>
2010-09-02 11:00:37 -0400

    added example project

---
+++ example_project/urls.py
@@ -0,0 +1,11 @@
+import os
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(os.path.dirname(__file__), 'media')}),
+)
